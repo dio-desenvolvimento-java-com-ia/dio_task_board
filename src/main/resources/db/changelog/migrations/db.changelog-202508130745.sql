973cedb99164c47167d1e18b8a2825d0b31affb1
renigoms <renan.nic28@gmail.com>
2025-08-13 08:32:10 -0300

    feat: new migrations

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202508130745.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset Renan:202508130745
+--comment: board_column table create
+
+CREATE TABLE BOARD_COLUMN(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    `order` INT NOT NULL,
+    kind VARCHAR(7),
+    board_id BIGINT NOT NULL,
+    CONSTRAINT boardTB__boards_column_fk FOREIGN KEY (board_id) REFERENCES BOARD_TABLE(id) ON DELETE CASCADE,
+    CONSTRAINT id_order_uk UNIQUE KEY unique_board_id_order (board_id, `order`)
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BOARDS
